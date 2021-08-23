c229e37c05e8da0f55950420bb6b10b877d25c53
VipulLGaikwad <gaikwadvipul19@gmail.com>
2021-08-23 21:48:51 +0530

    .

--- VipulGaikwad_111903126/GUI.py
+++ VipulGaikwad_111903126/GUI.py
@@ -4,9 +4,7 @@ window=Tk()
 
 window.title('Hello Python')
 window.geometry("300x200+10+20")
-#window.geometry("200x100")  
+
   
-b = Button(window,text = "Simple")  
   
-b.pack()  
 window.mainloop()
