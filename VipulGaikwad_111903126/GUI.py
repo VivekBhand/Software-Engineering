15e8f1615e84cf2028d6c8c03ed85b31204d30d5
VipulLGaikwad <gaikwadvipul19@gmail.com>
2021-08-23 22:26:17 +0530

    .

--- VipulGaikwad_111903126/GUI.py
+++ VipulGaikwad_111903126/GUI.py
@@ -1,15 +1,2 @@
-from tkinter import *
-window=Tk()
-# add widgets here
-
-window.title('Hello Python')
-window.geometry("300x200+10+20")
-
-c = Button(window,text = "Hard")  
-c.pack()
-
-b = Button(window,text = "Simple")
-
-b.pack() 
-  
-window.mainloop()
+if
+else
